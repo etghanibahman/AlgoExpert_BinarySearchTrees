de57f23ec3bddd8c443e7649b0643f1632018844
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2022-12-28 15:38:12 +0100

    Add project files.

---
+++ FindClosestValueInBst/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FindClosestValueInBst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        //public static int FindClosestValueInBst(BST tree, int target)
+        //{
+        //    // Write your code here.
+        //    return -1;
+        //}
+    }
+}
