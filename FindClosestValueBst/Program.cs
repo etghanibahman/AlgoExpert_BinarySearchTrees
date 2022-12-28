72051fc57b07c7b36dc61f0627deeca2414d3c02
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2022-12-28 16:19:46 +0100

    initial configs

---
+++ FindClosestValueBst/Program.cs
@@ -0,0 +1,29 @@
+﻿using SharedLogicTrees;
+using System;
+
+namespace FindClosestValueBst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree tree1 = new BinaryTree(10);
+            BinaryTree tree2 = new BinaryTree(5);
+            BinaryTree tree3 = new BinaryTree(15);
+            BinaryTree tree4 = new BinaryTree(2);
+            BinaryTree tree5 = new BinaryTree(5);
+            BinaryTree tree6 = new BinaryTree(13);
+            BinaryTree tree7 = new BinaryTree(22);
+
+            tree1.left = tree2;
+            tree1.right = tree3;
+            tree2.left = tree4;
+            tree2.right = tree5;
+            tree3.left = tree6;
+            tree3.right = tree7;
+
+            Helpers.PrintTree(tree1);
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
--- FindClosestValueInBst/Program.cs
+++ FindClosestValueInBst/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//using SharedLogicTrees;
+using System;
 
 namespace FindClosestValueInBst
 {
@@ -6,6 +7,23 @@ namespace FindClosestValueInBst
     {
         static void Main(string[] args)
         {
+
+            //BinaryTree tree1 = new BinaryTree(10);
+            //BinaryTree tree2 = new BinaryTree(5);
+            //BinaryTree tree3 = new BinaryTree(15);
+            //BinaryTree tree4 = new BinaryTree(2);
+            //BinaryTree tree5 = new BinaryTree(5);
+            //BinaryTree tree6 = new BinaryTree(13);
+            //BinaryTree tree7 = new BinaryTree(22);
+
+            //tree1.left = tree2;
+            //tree1.right = tree3;
+            //tree2.left = tree4;
+            //tree2.right = tree5;
+            //tree3.left = tree6;
+            //tree3.right = tree7;
+
+            //Helpers.PrintTree(tree1);
             Console.WriteLine("Hello World!");
         }
         //public static int FindClosestValueInBst(BST tree, int target)
